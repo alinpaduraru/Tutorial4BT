b73a88d8fffdda13ea47163968b0f52f63cc60fc
DESKTOP-CPSKCV5\alinp <alinpaduraru13@gmail.com>
2018-05-30 18:03:58 +0200

    120.2 Please work...

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "Public/PaduNEW_TankPlayerController.h"
-#include "BattleTank.h"
+#include "TankPlayerController.h"
+
+
 
 
--- BattleTank/Source/BattleTank/Public/TankPlayerController.h
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -2,22 +2,20 @@
 
 #pragma once
 
-
 #include "CoreMinimal.h"
-#include "Tank.h"
 #include "GameFramework/PlayerController.h"
-#include "PaduNEW_TankPlayerController.generated.h"
+#include "TankPlayerController.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class BATTLETANK_API APaduNEW_TankPlayerController : public APlayerController
+class BATTLETANK_API ATankPlayerController : public APlayerController
 {
 	GENERATED_BODY()
 
-public: 
 
+	
 	
 	
 };
