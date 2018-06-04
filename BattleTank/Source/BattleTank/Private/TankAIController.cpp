eaa22cc1762c6cc929b8f675fe961be87c4d4ee3
DESKTOP-CPSKCV5\alinp <alinpaduraru13@gmail.com>
2018-06-04 11:17:20 +0200

    126: Added BGM to Main Menu and Battleground, added 'tick' sounds on buttons hovered, AI Controller cpp class set

---
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -0,0 +1,49 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankAIController.h"
+#include "Runtime/Engine/Classes/GameFramework/Actor.h"
+
+
+ATank* ATankAIController::GetAIControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
+
+void ATankAIController::BeginPlay()
+{
+	Super::BeginPlay();
+	auto ControlledTank = GetAIControlledTank();
+	auto PlayerTank = GetPlayerTank();
+	//Getting the name of the controlled AI tank.
+	if (ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("TankAICOntroller.cpp: Name of the AI tank is: %s"), *ControlledTank->GetName());
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("TankAICOntroller.cpp: Not controlling any tank"))
+	}
+
+	//Getting player controller tank
+	if (PlayerTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("TankAICOntroller.cpp: Player controller Tank name is %s."),*PlayerTank->GetName())
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("TankAICOntroller.cpp: No PlayerControllerTank"))
+	}
+	
+}
+
+ATank* ATankAIController::GetPlayerTank() const
+{
+	auto PlayerPawn = GetWorld()->GetFirstPlayerController()->GetPawn();
+	if (!PlayerPawn)
+	{
+		return nullptr;
+	}
+	return Cast<ATank>(PlayerPawn);
+}
+
+
